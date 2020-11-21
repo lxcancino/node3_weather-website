6fe9f058a2943c07a04a4af2a77e7efe57ef6752
RUBEN CANCINO <rcancino@iMac-de-RUBEN.local>
2020-11-21 08:19:20 -0600

    setup for heroku

--- public/js/core.js
+++ public/js/core.js
@@ -1,4 +1,4 @@
-console.log('JavaScript for the browser....');
+console.log("JavaScript for the browser....");
 
 // fetch('http://localhost:3000/weather?address=Leon,Mexico')
 //   .then((response) => response.json())
@@ -7,19 +7,19 @@ console.log('JavaScript for the browser....');
 //   })
 //   .catch((reason) => console.error('Err: ', reason));
 
-const weatherForm = document.querySelector('form');
-const search = document.querySelector('input');
-const messageOne = document.querySelector('#messageOne');
-const messageTwo = document.querySelector('#messageTwo');
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#messageOne");
+const messageTwo = document.querySelector("#messageTwo");
 
 // messageOne.textContent = ' From JavaScript';
 
-weatherForm.addEventListener('submit', async (e) => {
+weatherForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   console.log(search.value);
-  messageOne.textContent = 'Loading...';
+  messageOne.textContent = "Loading...";
   const res = await fetchForecast(search.value);
-  messageOne.textContent = '';
+  messageOne.textContent = "";
   if (res.error) {
     messageTwo.textContent = res.error;
   } else {
@@ -27,15 +27,15 @@ weatherForm.addEventListener('submit', async (e) => {
     messageTwo.textContent = `
       Tem: ${temperature}\n
       Feels like: ${feelslike}\n
-      Description: ${weather_descriptions.join(',')}
+      Description: ${weather_descriptions.join(",")}
     `;
     messageOne.textContent = res.location;
   }
-  console.log('Forecast: ', res);
+  console.log("Forecast: ", res);
 });
 
 const fetchForecast = async (address) => {
-  const url = `http://localhost:3000/weather?address=${address}`;
+  const url = `/weather?address=${address}`;
   const response = await fetch(url);
   const fc = await response.json();
   return fc;
